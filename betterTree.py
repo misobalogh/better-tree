3b11d10ba4890b4877127869b55203d3505c6aca
Michal Balogh <baloghmichal03@gmail.com>
2024-06-07 16:48:23 +0200

    feat: displaying actual files and dirs

--- betterTree.py
+++ betterTree.py
@@ -1,16 +1,14 @@
 import os
 import argparse
 
-DEFAULT_DEPTH = 3
+DEFAULT_DEPTH = 1
 
-parser = argparse.ArgumentParser(description="🌳🌳🌳🌳")
-parser.add_argument('--depth', '-d', type=int,
-                    default=1, help='depth of the tree')
-parser.add_argument('--default', '-dd', type=int, default=3,
-                    help='default depth of the tree')
-parser.add_argument('--path', '-p', type=str,
-                    default=os.getcwd(), help='path to the directory')
-parser.add_argument('--fancy', '-f', action='store_true', help='fancy tree')
+parser = argparse.ArgumentParser(description="Tree command in Python.")
+parser.add_argument('--depth'  , '-d' , type=int, default=DEFAULT_DEPTH, help='depth of the tree')
+parser.add_argument('--default-depth', '-dd', type=int, default=DEFAULT_DEPTH, help='default depth of the tree')
+parser.add_argument('--path'   , '-p' , type=str, default=os.getcwd(),   help='path to the directory')
+parser.add_argument('--fancy'  , '-f' , action='store_true', help='fancy tree')
+parser.add_argument('--all'    , '-a' , action='store_true', help='show all files')
 
 args = parser.parse_args()
 
@@ -23,7 +21,7 @@ class Leaf:
     def __init__(self, name: str):
         self.name = name
 
-    def display(self, depth: int = 0, is_last: bool = False, prefix: str = ""):
+    def display(self, depth: int = DEFAULT_DEPTH, is_last: bool = False, prefix: str = ""):
         """
         Displays the node with appropriate indentation.
         """
@@ -46,7 +44,7 @@ class Tree(Leaf):
         """
         self.children.append(child)
 
-    def display(self, depth: int = 0, is_last: bool = False, prefix: str = ""):
+    def display(self, depth: int = 0, is_last: bool = False, prefix: str = "") -> None:
         """
         Displays the tree recursively with proper formatting.
         """
@@ -62,27 +60,28 @@ class Tree(Leaf):
             is_last_child = index == len(self.children) - 1
             child.display(depth + 1, is_last_child, prefix)
 
+    def createTree(self, path: str, depth: int, hidden=False):
+        """
+        Creates a tree structure of the given path with the given depth.
+        """
+        if depth == 0:
+            return self
+
+        with os.scandir(path) as entries:
+            files = [entry for entry in entries if (not entry.name.startswith('.')) or hidden]
+
+        for entry in files:
+            if entry.is_dir():
+                tree = Tree(entry.name)
+                self.add_child(tree)
+                tree.createTree(entry.path, depth - 1, hidden)
+            else:
+                leaf = Leaf(entry.name)
+                self.add_child(leaf)
+
+        return self
 
 
 if __name__ == "__main__":
-    root = Tree("Root")
-    child1 = Tree("Child 1")
-    child2 = Tree("Child 2")
-    child23 = Tree("Child 2.3")
-    leaf1 = Leaf("Leaf 1.1")
-    leaf2 = Leaf("Leaf 1.2")
-    leaf3 = Leaf("Leaf 2.1")
-    leaf4 = Leaf("Leaf 2.2")
-    leaf5 = Tree("Child 2.3")
-    leaf6 = Leaf("Leaf 2.3.1")
-
-    root.add_child(child1)
-    root.add_child(child2)
-    child1.add_child(leaf1)
-    child1.add_child(leaf2)
-    child2.add_child(leaf3)
-    child2.add_child(child23)
-    leaf5.add_child(leaf6)
-    child23.add_child(leaf5)
-    child2.add_child(leaf4)
-    root.display()
+    root = Tree(args.path)
+    root.createTree(args.path, args.depth, args.all).display()
